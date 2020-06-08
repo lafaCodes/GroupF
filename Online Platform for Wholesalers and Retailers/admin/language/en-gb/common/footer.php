ec500b3a1228f50dc62cf9c33a6d460902910fce
Roshan Thapa <53328329+r00sn@users.noreply.github.com>
2020-06-08 12:46:14 +1000

    Final Product v3.0

---
+++ Online Platform for Wholesalers and Retailers/admin/language/en-gb/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer']  = '<a href="http://localhost/groupf/onlineplatform/index.php">OnlinePlatform</a> &copy; 2020-' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Version %s';
---
+++ Online Platform for Wholesalers and Retailers/admin/language/en-gb/common/header.php
@@ -0,0 +1,12 @@
+<?php
+// Heading
+$_['heading_title']      = 'OnlinePlatform';
+
+// Text
+$_['text_profile']       = 'Your Profile';
+$_['text_store']         = 'Stores';
+$_['text_help']          = 'Help';
+$_['text_homepage']      = 'OnlinePlatform Poster';
+$_['text_support']       = 'Installation Manual';
+$_['text_documentation'] = 'User Manual';
+$_['text_logout']        = 'Logout';
